e8f42b83989e4c407086a3b1d27fbe469ca8ca99
shivaji <official.shivaji007@gmail.com>
2020-09-13 13:04:17 +0530

    Updates

---
+++ precache-manifest.7eb2491e7ba9de5fb7f8afaf0cd22712.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87af00b15c4adb171d0c67f6c278885",
+    "url": "/tikatok/index.html"
+  },
+  {
+    "revision": "cdc90e0e4125a13d2b6c",
+    "url": "/tikatok/static/css/main.fabcafaa.chunk.css"
+  },
+  {
+    "revision": "9be6b06dcb6c5b7da5be",
+    "url": "/tikatok/static/js/2.4c0f06b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tikatok/static/js/2.4c0f06b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc90e0e4125a13d2b6c",
+    "url": "/tikatok/static/js/main.ac42fdfc.chunk.js"
+  },
+  {
+    "revision": "087832ba78975eac9e46",
+    "url": "/tikatok/static/js/runtime-main.6450a769.js"
+  },
+  {
+    "revision": "b3b1cb700d9cf2ecc4e2fcc16f277b5c",
+    "url": "/tikatok/static/media/first.b3b1cb70.mp4"
+  },
+  {
+    "revision": "0a469f6779457746a9e9c2dc56c1188c",
+    "url": "/tikatok/static/media/second.0a469f67.mp4"
+  }
+]);
